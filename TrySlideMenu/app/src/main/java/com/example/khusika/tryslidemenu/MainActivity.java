399558da203289f3b6971e166faee9c4e5a4ee3a
khusika <khusikadhamar@gmail.com>
2015-11-22 20:31:22 +0700

    SlideMenu: Add WebView

--- TrySlideMenu/app/src/main/java/com/example/khusika/tryslidemenu/MainActivity.java
+++ TrySlideMenu/app/src/main/java/com/example/khusika/tryslidemenu/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.khusika.tryslidemenu;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.khusika.tryslidemenu.adapter.SlidingMenuAdapter;
 import com.example.khusika.tryslidemenu.fragment.fragment1;
@@ -25,6 +32,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView mWebView;
+    ProgressBar progressBar;
+
     private List<ItemSlideMenu> listSliding;
     private SlidingMenuAdapter adapter;
     private ListView listViewSliding;
@@ -36,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://khusika.github.io");
+        mWebView.setWebViewClient(new HelloWebViewClient());
 
         //Init component
         listViewSliding = (ListView) findViewById(R.id.lv_sliding_menu);
@@ -94,11 +113,84 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
     }
 
+    private class HelloWebViewClient extends WebViewClient{
+
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url)
+        {
+            webView.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(view.GONE);
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    { //if back key is pressed
+        if((keyCode == KeyEvent.KEYCODE_BACK)&& mWebView.canGoBack())
+        {
+            mWebView.goBack();
+            return true;
+
+        }
+
+        return super.onKeyDown(keyCode, event);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                MainActivity.this);
+
+        // set title
+        alertDialogBuilder.setTitle("Exit");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Do you really want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
 
     @Override
